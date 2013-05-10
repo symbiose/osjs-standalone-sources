2c5545ee89bf89caf6e50477f92640474c07c728
Anders Evenrud <andersevenrud@gmail.com>
2013-05-10 18:57:16 +0200

    Updated deps

--- install-deps.sh
+++ install-deps.sh
@@ -7,10 +7,11 @@
 #
 
 if [ -f /usr/bin/apt-get ]; then
-  apt-get install xorg xauth xcursor-themes
-  apt-get install alsa-utils pulseaudio pulseaudio-module-x11 consolekit dbus dbus-x11
-  apt-get install libwebkitgtk-3.0 libwebkitgtk-3.0-dev
+  apt-get install -y xorg xauth xcursor-themes
+  apt-get install -y alsa-utils pulseaudio pulseaudio-module-x11 consolekit dbus dbus-x11
+  apt-get install -y gstreamer0.10-alsa gstreamer0.10-pulseaudio gstreamer0.10-plugins-good gstreamer0.10-plugins-base
+  apt-get install -y libwebkitgtk-3.0 libwebkitgtk-3.0-dev
 elif [ -f /usr/bin/emerge ]; then
-  emerge xorg-server xauth xcursor-themes alsa pulseaudio libwebkitgtk consolekit dbus
+  emerge xorg-server xauth xcursor-themes alsa pulseaudio libwebkitgtk consolekit dbus gstreamer
 fi
 
